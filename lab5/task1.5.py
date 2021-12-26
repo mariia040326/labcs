84d9b846ef29885cff3dc8270edb865bf66b80a3
Mariia040326 <holysh.mariia@student.uzhnu.edu.ua>
2021-12-26 13:56:39 +0200

    task2.5

--- lab5/task1.5.py
+++ lab5/task1.5.py
@@ -1,10 +1,13 @@
 '''
 Дано дійсне число а. Обчислити a(a+1)...(a+n-1)
 '''
-a=2
+'''a=2
 n=3
 dob=1
 for i in range(n+1):
     c=a+i-1
     dob*=c
-print(dob)
+print(dob)'''
+a=input('a=')
+n=len(a)
+print(n)
