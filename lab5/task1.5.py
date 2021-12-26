f2a341b4928f0faf4bb74866be35fadc6d0afca4
Mariia040326 <holysh.mariia@student.uzhnu.edu.ua>
2021-12-26 14:11:10 +0200

    task3.5

--- lab5/task1.5.py
+++ lab5/task1.5.py
@@ -8,6 +8,15 @@ for i in range(n+1):
     c=a+i-1
     dob*=c
 print(dob)'''
-a=input('a=')
-n=len(a)
-print(n)
+import math
+e=10
+x=3
+s=0
+for i in range(e+1):
+    a = (1 / 2 * i - 1) * math.pow(((x - 1) / (x + 1)), (2 * i - 1))
+    s+=a
+print(s+2)
+if math.log1p(x)==(s+2):
+    print('рівність виконується')
+else:
+    print('рівність не виконується')
